f9984bc6c4c27668735f1f6cddb579a30f6ab81d
Pawel Chmielewski <p.t.chmielewski@gmail.com>
2025-12-10 09:57:56 +0100

    feat: Updated assert-benchmark script, removed unnecessary code, changed func visiblity

--- benchmark-assert.sh
+++ benchmark-assert.sh
@@ -110,8 +110,8 @@ run_benchmark_assertion "BenchmarkLibraryNative_HashSHA_512" 3200 5
 run_benchmark_assertion "BenchmarkLibraryNative_HashSHA_512_256" 4100 5
 run_benchmark_assertion "BenchmarkLibraryNative_HashShake_128" 4500 12
 run_benchmark_assertion "BenchmarkLibraryNative_HashShake_256" 4800 12
-run_benchmark_assertion "BenchmarkLibraryNative_SignCertificate_NIST_SECP521R1_RSA4096" 7000000 400
-run_benchmark_assertion "BenchmarkLibraryNative_SignCertificate_NIST_SECP521R1_NIST_SECP521R1" 7000000 400
+run_benchmark_assertion "BenchmarkLibraryNative_SignCertificate_NIST_SECP521R1_RSA4096" 7000000 501
+run_benchmark_assertion "BenchmarkLibraryNative_SignCertificate_NIST_SECP521R1_NIST_SECP521R1" 7000000 501
 
 echo ""
 echo "All benchmark assertions passed!"
