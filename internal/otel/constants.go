2110529eb251410e33c9317466a626ced8fc9999
Paweł Chmielewski <p.t.chmielewski@gmail.com>
2026-01-29 15:02:14 +0100

    feat: Introduced OTEL constants, updated Taskfile, updated tracer

---
+++ internal/otel/constants.go
@@ -0,0 +1,28 @@
+package otel
+
+// default values for OTEL configurations
+const (
+	defaultServiceName    = "crypto-broker-server"
+	defaultServiceVersion = "unknown service version"
+	defaultTracesExporter = "console"
+)
+
+// keys representing OTEL exporters
+const (
+	keyExporterOTLPGRPC = "otlpgrpc"
+	keyExporterConsole  = "console"
+	keyExporterOTLPHTTP = "otlphttp"
+)
+
+// sampler names
+const (
+	samplerAlwaysOn = "always_on"
+	samplerAlways = "always"
+	samplerAlwaysOff = "always_off"
+	samplerNever = "never"
+	samplerTraceIDRatio = "traceidratio"
+	samplerRatio = "ratio"
+	samplerParentBasedAlwaysOn = "parentbased_always_on"
+	samplerParentBasedAlwaysOff = "parentbased_always_off"
+	samplerParentBasedTraceIDRatio = "parentbased_traceidratio"
+)
