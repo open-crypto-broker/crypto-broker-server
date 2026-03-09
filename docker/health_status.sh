b1d0f61083b9d1ed923374015381bbd88b72c408
Paweł Chmielewski <p.t.chmielewski@gmail.com>
2026-03-09 14:29:26 +0100

    fix: Updated socker file name

--- docker/health_status.sh
+++ docker/health_status.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Call gRPC service and capture JSON output
-output=$(grpcurl -proto health.proto -plaintext -d '{"service":""}' unix:/tmp/open-crypto-broker/cryptobroker.sock grpc.health.v1.Health/Check 2>/dev/null)
+output=$(grpcurl -proto health.proto -plaintext -d '{"service":""}' unix:/tmp/open-crypto-broker/crypto-broker-server.sock grpc.health.v1.Health/Check 2>/dev/null)
 
 # Extract the "status" field using grep + sed
 status=$(echo "$output" | grep -o '"status"[[:space:]]*:[[:space:]]*"[^"]*"' | sed 's/.*: *"\(.*\)"/\1/')
